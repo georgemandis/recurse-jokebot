2fb2d9aa4217035429d89aaacbf938e591aeedaf
George Mandis <george@mand.is>
2020-01-13 17:53:53 -0500

    up/down voting now works

--- jokebot.py
+++ jokebot.py
@@ -19,18 +19,40 @@ class JokeBotHandler(object):
 
     def handle_message(self, message, bot_handler):
         content = self.usage()
+
         # storage = bot_handler.storage
         jokes = self.get_jokes()
-
-        # if storage.contains('jokes'):
-        #     jokes = storage.get('jokes')        
-
+    
         msg_content = message['content'].lower()
+        
+        if msg_content.startswith("up") or msg_content.startswith("down"):
+            try:
+                command = msg_content.split()
+                joke_index = int(command[1].strip())
+                direction = command[0].strip()
+
+                if joke_index > len(jokes) or joke_index < 0:
+                    content = "I only have {} jokes".format(len(jokes))
+                else:
+                    if direction == 'up':                        
+                        jokes[joke_index]['score'] = jokes[joke_index]['score'] + 1
+                    else:
+                        jokes[joke_index]['score'] = jokes[joke_index]['score'] - 1    
+                    
+                    self.save_jokes(jokes)
+
+                    content = "Score for joke #{} is now {}".format(joke_index, jokes[joke_index]['score'])
+                    # vote the joke up or down
+
+                    
+            except:
+                content = self.usage()   
+
         if msg_content.startswith("add"):
             new_joke = message['content'].replace("add", "").replace('\\n', '\n').strip()
             new_joke_dict = {
                 "joke": new_joke,
-                "submitted_by": message['sender_email'],
+                "submitted_by": message.get('sender_full_name', (message.get('sender_email', '?????jo')) ) ,
                 "score": 10
             }
             jokes.append(new_joke_dict)
@@ -53,9 +75,10 @@ class JokeBotHandler(object):
             
             if len(jokes) == 0:
                 content = "I don't know any jokes yet. Teach me some!"
-            else:            
-                random_joke = random.randint(0, len(jokes) - 1)
-                content = "Joke #{}:\n\n{}".format(random_joke, jokes[random_joke]['joke'])
+            else:
+                good_jokes = [ d for d in jokes if d['score'] > 0 ]
+                random_joke = random.randint(0, len(good_jokes) - 1)
+                content = "Joke #{}:\n\n{}".format(random_joke, good_jokes[random_joke]['joke'])
 
         bot_handler.send_reply(message, content)
 
